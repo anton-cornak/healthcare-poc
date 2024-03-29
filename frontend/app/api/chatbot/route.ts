32acf5eef45612dae5fee2b69bec0d1cd18f3493
“anton-cornak” <anton.cornak@visma.com>
2024-03-29 21:58:30 +0100

    small things

--- frontend/app/api/chatbot/route.ts
+++ frontend/app/api/chatbot/route.ts
@@ -37,9 +37,7 @@ async function postToServer(endpoint: string, body: string): Promise<Response> {
 }
 
 export async function POST(req: Request): Promise<Response> {
-	const body: {
-		message: string;
-	} = await req.json();
+	const body = await req.json();
 
 	if (!body.message) {
 		return new Response(JSON.stringify({ message: "Bad request" }), {
@@ -48,53 +46,43 @@ export async function POST(req: Request): Promise<Response> {
 		});
 	}
 
-	let endpoint: string;
-	let bodyToServer: string;
-
 	try {
-		const res = await postOpenAI([{ role: "user", content: body.message }]);
+		const openAIResponse = await postOpenAI([
+			{ role: "user", content: body.message },
+		]);
+		const { choices } = await openAIResponse.json();
+		const choice = choices[0];
+		console.log;
 
-		const parsedRes = await res.json();
-		endpoint = parsedRes.choices[0].message.function_call.name;
-		bodyToServer = parsedRes.choices[0].message.function_call.arguments;
-	} catch (error) {
-		console.error("Error in postOpenAI:", error);
-		return new Response(
-			JSON.stringify({ message: "Internal Server Error" }),
-			{
-				status: 500,
-				headers: { "Content-Type": "application/json" },
-			},
-		);
-	}
+		if (!choice || !choice.message) {
+			throw new Error("Invalid response from OpenAI");
+		}
 
-	let serverResult: any;
+		// Handle direct message responses
+		if (!choice.message.function_call) {
+			return new Response(
+				JSON.stringify({ message: choice.message.content }),
+				{
+					status: 200,
+					headers: { "Content-Type": "application/json" },
+				},
+			);
+		}
 
-	try {
-		const res = await postToServer(endpoint, bodyToServer);
+		const { name: endpoint, arguments: bodyToServer } =
+			choice.message.function_call;
 
-		serverResult = await res.json();
-	} catch (error) {
-		console.error("Error in postOpenAI:", error);
-		return new Response(
-			JSON.stringify({ message: "Internal Server Error" }),
-			{
-				status: 500,
-				headers: { "Content-Type": "application/json" },
-			},
-		);
-	}
+		// Send request to the determined server endpoint
+		const serverResponse = await postToServer(endpoint, bodyToServer);
+		const serverResult = await serverResponse.json();
 
-	try {
-		const res = await postOpenAI([
+		// Return processed result back to the user
+		const finalResponse = await postOpenAI([
 			{ role: "user", content: body.message },
 			{
 				role: "assistant",
 				content: null,
-				function_call: {
-					name: endpoint,
-					arguments: bodyToServer,
-				},
+				function_call: { name: endpoint, arguments: bodyToServer },
 			},
 			{
 				role: "function",
@@ -103,16 +91,16 @@ export async function POST(req: Request): Promise<Response> {
 			},
 		]);
 
-		const parsedRes = await res.json();
+		const finalResult = await finalResponse.json();
 		return new Response(
-			JSON.stringify({ message: parsedRes.choices[0].message.content }),
+			JSON.stringify({ message: finalResult.choices[0].message.content }),
 			{
 				status: 200,
 				headers: { "Content-Type": "application/json" },
 			},
 		);
 	} catch (error) {
-		console.error("Error in postOpenAI:", error);
+		console.error("Error:", error);
 		return new Response(
 			JSON.stringify({ message: "Internal Server Error" }),
 			{
--- frontend/components/Chat.tsx
+++ frontend/components/Chat.tsx
@@ -1,43 +1,52 @@
 "use client";
-import { useState, useRef, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
+
+interface Message {
+	id: number;
+	text: string;
+	sender: "user" | "bot";
+}
 
 export default function Chat() {
-	const [messages, setMessages] = useState<string[]>([]);
+	const [messages, setMessages] = useState<Message[]>([]);
 	const [userMessage, setUserMessage] = useState<string>("");
 	const chatContainerRef = useRef<HTMLDivElement | null>(null);
 
+	useEffect(() => {
+		setMessages([
+			{
+				id: Date.now(),
+				text: "Dobrý deň. Moje meno je Zidan Sufurki a som váš asistent. Mojou úlohou je pomôcť Vám pri hľadaní lekára vo vašom okolí. Na začiatok mi, prosím, napíšte, akého lekára hľadáte a kde sa nachádzate.",
+				sender: "bot",
+			},
+		]);
+	}, []);
+
+	const addMessage = (text: string, sender: "user" | "bot") => {
+		setMessages((prevMessages) => [
+			...prevMessages,
+			{ id: Date.now(), text, sender },
+		]);
+	};
+
 	const handleUserMessage = async () => {
 		if (userMessage.trim() !== "") {
-			setMessages((prevMessages) => [
-				...prevMessages,
-				`You: ${userMessage}`,
-			]);
+			addMessage(userMessage, "user");
 
 			const botResponse = await fetch("/api/chatbot", {
 				method: "POST",
 				body: JSON.stringify({ message: userMessage }),
 			}).then((response) => response.json());
 
-			setMessages((prevMessages) => [
-				...prevMessages,
-				`Bot: ${botResponse.message}`,
-			]);
+			addMessage(botResponse.message, "bot");
 
 			setUserMessage("");
 		}
 	};
 
-	const handleDeleteMessage = (indexToDelete: number) => {
-		const updatedMessages = messages.filter(
-			(_, index) => index !== indexToDelete,
-		);
-		setMessages(updatedMessages);
-	};
-
 	const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
 		if (e.key === "Enter") {
 			handleUserMessage();
-			setUserMessage("");
 		}
 	};
 
@@ -50,23 +59,29 @@ export default function Chat() {
 
 	return (
 		<div className="min-h-screen flex justify-center items-center">
-			<div className="bg-gray-200 p-4 w-[70vh]">
+			<div className="bg-gray-200 p-4 w-full max-w-md">
 				<div
 					className="bg-white rounded shadow-lg p-4 h-[60vh] overflow-y-auto"
 					ref={chatContainerRef}
 				>
-					{messages.map((message, index) => (
+					{messages.map((message) => (
 						<div
-							key={index}
-							className="mb-2 flex items-center justify-between"
+							key={message.id}
+							className={`mb-2 flex ${
+								message.sender === "user"
+									? "justify-end"
+									: "justify-start"
+							}`}
 						>
-							<div>{message}</div>
-							<button
-								className="text-red-600 hover:text-red-800"
-								onClick={() => handleDeleteMessage(index)}
+							<div
+								className={`rounded px-4 py-2 ${
+									message.sender === "user"
+										? "bg-blue-500 text-white"
+										: "bg-gray-300 text-black"
+								}`}
 							>
-								Delete
-							</button>
+								{message.text}
+							</div>
 						</div>
 					))}
 				</div>
@@ -80,7 +95,7 @@ export default function Chat() {
 						onKeyDown={handleKeyPress}
 					/>
 					<button
-						className="rounded-r-lg p-2 bg-blue-500 text-white hover:bg-blue-600"
+						className="rounded-r-lg px-4 bg-blue-500 text-white hover:bg-blue-600"
 						onClick={handleUserMessage}
 					>
 						Send
